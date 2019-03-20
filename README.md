f9946d76f44be2eda29ff129ec2ff7871c70ab44
yudi <ude.hirosue@gmail.com>
2019-03-20 16:08:14 +0700

    --seeder added

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # laravel-spatie-permission
 permission dan role untuk laravel menggunakan spatie-permission
 
+# installation steps
+- clone repo https://github.com/udehiro/laravel-spatie-permission.git
+- composer install
+- cp .env.example .env
+- set environment database at .env file
+- php artisan key:generate
+- php artisan migrate
+- php artisan db:seed
+
+# usage
+- php artisan serve
+- username : admin, password : admin
+- [nge-teh sambil makan gorengan]
+
 <p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
 
 <p align="center">
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,66 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'id' => '1',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => '1',
+            'name' => 'Admin',
+            'guard_name' => 'web'
+        ]);
+
+        DB::table('permissions')->insert([
+            [
+                'id' => '1',
+                'name' => 'dashboard',
+                'guard_name' => 'web'
+            ], [
+                'id' => '2',
+                'name' => 'data.read',
+                'guard_name' => 'web'
+            ], [
+                'id' => '3',
+                'name' => 'data.create',
+                'guard_name' => 'web'
+            ], [
+                'id' => '4',
+                'name' => 'data.delete',
+                'guard_name' => 'web'
+            ], [
+                'id' => '5',
+                'name' => 'data.update',
+                'guard_name' => 'web'
+            ]
+        ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => '1',
+            'model_type' => 'App\User',
+            'model_id' => '1'
+        ]);
+
+        DB::table('role_has_permissions')->insert([
+            [
+                'permission_id' => '1',
+                'role_id' => '1'
+            ], [
+                'permission_id' => '2',
+                'role_id' => '1'
+            ], [
+                'permission_id' => '3',
+                'role_id' => '1'
+            ], [
+                'permission_id' => '4',
+                'role_id' => '1'
+            ], [
+                'permission_id' => '5',
+                'role_id' => '1'
+            ]
+        ]);
     }
 }
